5383320d3708e4992ffc296ee26cc6446c4c6915
シュール <shule517@gmail.com>
2017-07-15 12:41:58 +0900

    どこからどうやって情報を取ろうかな

---
+++ spec/test_spec.rb
@@ -0,0 +1,28 @@
+describe Art do
+
+  # 現在情報
+  # http://standingpine.jp/exhibitions/ # 現在(最新の展覧会の詳細ページ)
+  # http://galleryhu.com/exhibitions/ # 全て 一覧 最初の１件
+  # http://1210.g-ham.com/exhibitions/ # 全て 一覧 最初の１件
+  # http://masayoshisuzukigallery.com/press/exhibitions_s.html # 現在(最新の展覧会の詳細ページ)
+  # http://www.kenjitaki.com/pages/intro1_j.html # １ページに２件ある
+
+  # 過去情報なら一覧取れそう
+  # http://standingpine.jp/past/ # 過去
+  # http://galleryhu.com/exhibitions/ # 全て
+  # http://1210.g-ham.com/exhibitions/ # 全て
+  # http://masayoshisuzukigallery.com/art/exhibitions_a.html # 過去
+  # http://www.kenjitaki.com/exhibitions/past_tj15.html # 2015-2017
+
+  it 'タイトル' do
+  end
+
+  it '会期（いつからいつまでか）' do
+  end
+
+  it 'メインイメージ' do
+  end
+
+  it '内容' do
+  end
+end
